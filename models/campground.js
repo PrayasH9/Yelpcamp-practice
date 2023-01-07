21631f137f27a36f75cd6edd4fd4f636575f9549
PrayasH9 <jantyhansa@gmail.com>
2023-01-07 19:35:12 +0530

    cloudinary url is changed to make images more optimized and new allowedFormats are added in the cloudinary folder

--- models/campground.js
+++ models/campground.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
-const Review = require('./review');
+const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    url: String,
-    filename: String
-})
+  url: String,
+  filename: String,
+});
 
-ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/q_auto/w_auto/dpr_auto/f_auto");
 });
 
 const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+  title: String,
+  images: [ImageSchema],
+  price: Number,
+  description: String,
+  location: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+  ],
 });
 
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if (doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
-})
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
